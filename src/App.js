682ca82a72d7bebd78ae5b0fe063218370736117
Sergey <sergeygetman2020@gmail.com>
2021-10-23 20:37:25 +0300

    first commit redux connect react state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {connect} from "react-redux";
+import {setBooks} from "./actions/books";
 
-function App() {
+
+
+function App(props) {
+  console.log(props)
+  const {books} = props.books
+  const {setBooks} = props
+  const newBooks = [{
+    id : 1,
+    title : " Новое время: " + new Date()
+  }]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Привет {books[0].title} </h1>
+      <button onClick={setBooks.bind(this, newBooks)}>SET new BOOKS</button>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => ({
+  ...state
+})
+
+const mapDispatchToProps = dispatch => ({
+setBooks : books => dispatch(setBooks(books))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
